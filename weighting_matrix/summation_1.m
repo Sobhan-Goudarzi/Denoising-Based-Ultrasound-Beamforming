46bfb9eaedb25ac7b8b09a557e2ebbd6516344fd
Sobhan Goudarzi <82527379+Sobhan-Goudarzi@users.noreply.github.com>
2022-08-09 17:40:51 -0400

    Add files via upload

---
+++ weighting_matrix/summation_1.m
@@ -0,0 +1,15 @@
+clc
+clear all
+close all
+warning off
+%%
+count = 0;
+for i=1:8
+    B_k = sparse(195456,484137);% update the matrix size based on your selections
+    for j=1:16
+        count = count+1;
+        load(['A_k_',num2str(count),'.mat'])
+        B_k = B_k+A_k;
+    end
+    save(['B_k_',num2str(i)],'B_k')
+end
---
+++ weighting_matrix/summation_2.m
@@ -0,0 +1,12 @@
+clc
+clear all
+close all
+warning off
+%%
+count = 0;
+Phi = sparse(195456,484137);% update the matrix size based on your selections
+for i=1:8
+    load(['B_k_',num2str(i),'.mat'])
+    Phi = Phi+B_k;
+end
+save('Phi','Phi')
